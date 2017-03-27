c72ae1fc414895919ef71ae370686353d9086bce
Laurensius Adi <laurensiusadi@gmail.com>
2017-03-27 16:58:31 +0700

    Service worker update
    Update working to cache and fetch all files needed. But resulting in a
    longer loading time since app loads same file 2 times, from service
    worker and requesting once again from external links.

--- public/sw.js
+++ public/sw.js
@@ -1,44 +1,40 @@
 var cacheName = 'v1';
-
-var assetsToCache = [
+var dataCacheName = 'data-blog-1';
+var filesToCache = [
 //list of cache here
 ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-        return cache.addAll(assetsToCache);
-    }).then(function() {
-      return self.skipWaiting();
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
     })
   );
 });
-
-self.addEventListener('activate', function(event) {
-    return self.clients.claim();
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+  }).then(function(){
+      console.log('[ServiceWorker] Claiming clients for version', cacheName);
+      return self.clients.claim();
+  })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  // Ignore non-get request like when accessing the admin panel
-  if (event.request.method !== 'GET') { return; }
-  // Don't try to handle non-secure assets because fetch will fail
-  if (/http:/.test(event.request.url)) { return; }
-
-  // Here's where we cache all the things!
-  event.respondWith(
-    // Open the cache created when install
-    caches.open(cacheName).then(function(cache) {
-      // Go to the network to ask for that resource
-      return fetch(event.request).then(function(networkResponse) {
-        // Add a copy of the response to the cache (updating the old version)
-        cache.put(event.request, networkResponse.clone());
-        // Respond with it
-        return networkResponse;
-      }).catch(function() {
-        // If there is no internet connection, try to match the request
-        // to some of our cached resources
-        return cache.match(event.request);
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+      caches.match(e.request).then(function(response) {
+          return response || fetch(e.request);
       })
-    })
   );
 });
